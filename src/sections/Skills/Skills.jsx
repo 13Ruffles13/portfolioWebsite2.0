06e69b07ff880969e4f5facf04dca8b169474cb2
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2024-07-11 22:01:10 -0700

    Skill section
    Content up to date regarding skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -15,30 +15,64 @@ function Skills() {
 
   return (
     <section id="skills" className={styles.container}>
-      <h1 className="sectionTitle">Skills</h1>{" "}
-      {/* Main title for the skills section */}
-      {/* Skill lists categorized into sections */}
+      <h1 className="sectionTitle">Skills</h1>
+
+      {/* Programming Languages */}
+      <h2>Programming Languages</h2>
       <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="Swift" />
+      </div>
+      <hr />
+
+      {/* Front-End Development */}
+      <h2>Front-End Development</h2>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="JavaScript (ES6)" />
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="jQuery" />
+        <SkillList src={checkMarkIcon} skill="Bootstrap" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="ReactJS" />
+        <SkillList src={checkMarkIcon} skill="AJAX (REST)" />
+        <SkillList src={checkMarkIcon} skill="Git" />
       </div>
       <hr />
+
+      {/* Back-End Development */}
+      <h2>Back-End Development</h2>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="Express.js" />
+        <SkillList src={checkMarkIcon} skill="Database Management (SQL)" />
       </div>
       <hr />
+
+      {/* Frameworks and Libraries */}
+      <h2>Frameworks and Libraries</h2>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
-        <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Jasmin" />
+        <SkillList src={checkMarkIcon} skill="Babel" />
         <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+      </div>
+      <hr />
+
+      {/* Software Development Fundamentals */}
+      <h2>Software Development Fundamentals</h2>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Object-Oriented Programming" />
+        <SkillList src={checkMarkIcon} skill="Data Structures and Algorithms" />
+        <SkillList src={checkMarkIcon} skill="Version Control (Git)" />
+        <SkillList src={checkMarkIcon} skill="Problem Solving" />
+        <SkillList src={checkMarkIcon} skill="Agile Methodologies" />
+        <SkillList
+          src={checkMarkIcon}
+          skill="Collaboration and Communication"
+        />
       </div>
       <hr />
     </section>
